b8471f4049b1f2efc7618e1321af930240ad1043
Justin <justinherrera013@yahoo.com>
2023-01-15 02:13:46 +0800

    working sending email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,31 +1,89 @@
-import { RefObject } from 'react';
+import { useState, useRef, RefObject } from 'react';
 import calling from '../assets/calling.svg'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+import emailjs from '@emailjs/browser';
 
 interface ContactProps {
   contactRef?: RefObject<HTMLDivElement>
 }
 
+interface AxiosBody {
+  service_id: string
+  template_id: string
+  user_id: string
+}
+
 const Contact = ({ contactRef }: ContactProps) => {
+  const [inputForm, setInputForm] = useState({
+    name: '',
+    email: '',
+    message: 'Hello'
+  })
+  const form = useRef<HTMLFormElement>(null)
+
+  const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = form.current
+    emailjs.sendForm("service_glsshy3", "template_r0zpf68", data || "", "OAC9NWYxt00An1stn")
+      .then(() => {
+        setInputForm({
+          name: '',
+          email: '',
+          message: ''
+        })
+        notify()
+      }).catch(error => {
+        console.log(error)
+      })
+  }
+
+  const notify = () => {
+    toast.success('Email Sent', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+
   return (
     <div className="bg-white h-screen pt-20 pl-10 md:pl-20 pr-10 md:pr-20" id="contact-page" ref={contactRef}>
+      <ToastContainer />
       <p className="text-7xl text-center">Contact Me</p>
       <p className="text-center">Let's work together or drop me a message and say hello</p>
       <div className="pt-10 flex flex-col md:flex-row justify-center">
         <div className="basis-1/2 md:p-8">
-          <div className="mb-2">
-            <label>Name:</label>
-            <input type="text" className="block p-2 border rounded border-[#191919] w-[100%]" />
-          </div>
-          <div className="mb-2">
-            <label>Email:</label>
-            <input type="email" className="block p-2 border rounded border-[#191919] w-[100%]" />
-          </div>
-          <div className="">
-            <label>Message:</label>
-            <textarea name="message" className="block p-2 border rounded border-[#191919] w-[100%]" />
-            {/* <input type="email" className="block p-2 border rounded border-[#191919] w-[100%]" /> */}
-          </div>
-          <button className="p-2 mt-6 bg-[#191919] text-white rounded hover:bg-white hover:text-[#191919] hover:border-2 hover:border-[#191919]">Submit</button>
+          <form ref={form} onSubmit={sendEmail}>
+            <div className="mb-2">
+              <label>Name:</label>
+              <input type="text" className="block p-2 border rounded border-[#191919] w-[100%]" name="from_name" value={inputForm.name} onChange={(e) => setInputForm({
+                ...inputForm,
+                name: e.target.value
+              })} required/>
+            </div>
+            <div className="mb-2">
+              <label>Email:</label>
+              <input type="email" className="block p-2 border rounded border-[#191919] w-[100%]" name="from_email"  value={inputForm.email} onChange={(e) => setInputForm({
+                ...inputForm,
+                email: e.target.value
+              })} required/>
+            </div>
+            <div className="">
+              <label>Message:</label>
+              <textarea name="message" className="block p-2 border rounded border-[#191919] w-[100%]" value={inputForm.message} onChange={(e) => setInputForm({
+                ...inputForm,
+                message: e.target.value
+              })} required/>
+              {/* <input type="email" className="block p-2 border rounded border-[#191919] w-[100%]" /> */}
+            </div>
+            <button type="submit" className="p-2 mt-6 bg-[#191919] text-white rounded hover:bg-white hover:text-[#191919] hover:border-2 hover:border-[#191919]" >Submit</button>
+          </form>
+          
         </div>
         <div className="basis-1/2 p-8 hidden md:block">
           <img src={calling} height={300} width={600} className="mx-auto" />
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,16 +8,16 @@ interface ProjectsProps {
 
 const Projects = ({ projectsRef }: ProjectsProps) => {
   return (
-    <div className="bg-[#191919] h-auto md:h-screen w-[100vw]" id="projects-page" ref={projectsRef}>
-      <div className="flex flex-col h-full pt-12">
+    <div className="bg-[#191919] h-auto md:h-screen w-[100vw] py-12" id="projects-page" ref={projectsRef}>
+      <div className="flex flex-col h-full">
         <p className="text-white text-7xl text-center">Recent Works</p>
-        <div className="flex flex-col md:flex-row md:space-x-10 justify-center items-center">
-          {[1,2,3].map((element) => (
-            <div className="works-div flex flex-row mt-10 cursor-pointer transition duration-700 ease-in-out hover:scale-110">
+        <div className="flex flex-col md:flex-row md:space-x-10 justify-center items-center w-[100%] flex-wrap">
+          {[1,2,3,4].map((element) => (
+            <div className="works-div flex flex-row mt-10 cursor-pointer transition duration-700 ease-in-out hover:scale-110 text-center md:w-1/3">
               <div className="border-1 p-4 border border-white rounded">
                 <p className="text-2xl text-white">Customer Feedback App</p>
-                <small className="text-white">Tech Used: </small>
-                <img src={mj_src} height={300} width={300}/>
+                <small className="text-white">Tech Used: React + AWS Serverless (API GW, Lambda, DynamoDB)</small>
+                <img src={mj_src} height={300} width={400} />
                 <div className="options-div flex flex-row justify-evenly mt-4">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#ffffff" className="w-6 h-6 cursor-pointer hover:border-b-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
